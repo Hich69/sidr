307e880c17765411357b91858f4b5fbe8c59c802
Hecham AZRAK <hecham.azrak@echosens.com>
2019-06-18 11:10:44 +0200

    data_example modified, Summarize modified

--- create_data.R
+++ create_data.R
@@ -2,7 +2,8 @@
 dat = data.frame(USUBJID = 1:1000,
                  VALUE = runif(n = 1000, min = 0.5, max = 0.75),
                  GLD = rbinom(n = 1000, size = 1, prob = 0.5))
+data_test <- dat
 
-save(dat, file = "data/data_test.RData")
+#save(data_test, file = "data/data_test.RData")
 
-devtools::use_data(dat)
+devtools::use_data(data_test, overwrite = TRUE)
